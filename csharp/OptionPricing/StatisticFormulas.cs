2d634eec2bf0421cba7a0810ddcfc362e0a28e50
l.baduet <l.baduet@axisalternatives.com>
2024-01-27 12:50:30 +0100

    first Csahrp code

---
+++ csharp/OptionPricing/StatisticFormulas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ValoLibrary {
+    public class StatisticFormulas{
+        public static double linearInterpol(double x, double x0, double x1, double y0, double y1) {
+            if ((x1 - x0) == 0) {
+                return (y0 + y1) / 2;
+            }
+
+            return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+        }
+
+        public static double NormalDensity(double x, double mean, double std)
+        {
+            return 1 / (std * Math.Sqrt(2.0 * Math.PI)) * Math.Exp(-0.5 * (Math.Pow((x - mean) / std, 2)));
+        }
+        public static double Cfd(double x)
+        {
+            const double a1 = 0.254829592;
+            const double a2 = -0.284496736;
+            const double a3 = 1.421413741;
+            const double a4 = -1.453152027;
+            const double a5 = 1.061405429;
+            const double p = 0.3275911;
+            int sign = 1;
+            if (x < 0) sign = -1;
+            x = Math.Abs(x) / Math.Sqrt(2.0);
+
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return 0.5 * (1.0 + sign * y);
+        }
+
+        public static double RandomNormal(double mean, double stddev)
+        {
+            // The method requires sampling from a uniform random of (0,1]
+            // but Random.NextDouble() returns a sample of [0,1).
+
+            var random = new Random();
+            double x1 = 1 - random.NextDouble();
+            double x2 = 1 - random.NextDouble();
+
+            double y1 = Math.Sqrt(-2.0 * Math.Log(x1)) * Math.Cos(2.0 * Math.PI * x2);
+            return y1 * stddev + mean;
+        }
+
+        static public double linearInterpolation(double x, double x0, double x1, double y0, double y1)
+        {
+            if ((x1 - x0) == 0)
+            {
+                return (y0 + y1) / 2;
+            }
+            return y0 + (x - x0) * (y1 - y0) / (x1 - x0);
+        }
+
+    }
+
+}
---
+++ csharp/OptionPricing/UDF.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace ValoLibrary
+{
+    [ComVisible(true)]
+
+    public interface IUDF
+    {
+        double CallPrice(double K, double T, string underlying);
+        double PutPrice(double K, double T, string underlying);
+        double test();
+        double Sensibility(double K, double T, string underlying);
+    }
+    [ComVisible(true)]
+    [ProgId("ValoLibrary.UDF")]
+    [Guid("122D3198-A973-4DCF-85A3-8A4C81C12733")]
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    public class UDF : IUDF
+    {          
+        public double test() 
+        { 
+            return 7;
+        }
+
+        public double Sensibility(double K, double T, string underlying)
+        {
+            double S = GetData.GetSpot(underlying);
+            double r = Calibration.GetRepo(underlying, T);
+            double date = GetData.GetTime(underlying);
+            double timeToMaturity = (T - date) / 360;
+            double price = Calibration.interpolatePrice(K, T, underlying);
+            double vol = Math.Abs(BlackScholes.ImpliedVol('c', S, price, r, K, timeToMaturity));
+            double sensitivity = BlackScholes.Vega('c',S,vol,r,K, timeToMaturity);
+            return sensitivity;
+        }
+        public double CallPrice(double K, double T, string underlying)
+        {
+            double S =  GetData.GetSpot(underlying);
+            double r = Calibration.GetRepo(underlying, T);
+            double date =  GetData.GetTime(underlying);
+            double timeToMaturity = (T - date)/360;
+            double price = Calibration.interpolatePrice(K, T,underlying);
+            double vol = BlackScholes.ImpliedVol('c', S, price, r, K, timeToMaturity);
+            double P = BlackScholes.Price('c', S, vol, r, K, timeToMaturity);
+            return P;
+        }
+        public double PutPrice(double K, double T, string underlying)
+        {
+            double S = GetData.GetSpot(underlying);
+            double r = Calibration.GetRepo(underlying, T);
+            double date = GetData.GetTime(underlying);
+            double timeToMaturity = (T - date) / 360;
+            double price = Calibration.interpolatePrice(K, T, underlying);
+            double vol = BlackScholes.ImpliedVol('p', S, price, r, K, timeToMaturity);
+            double P = BlackScholes.Price('p', S, vol, r, K, timeToMaturity);
+            return P;
+        }
+    }
+}
